765def1aaaede4b68c0922b1ccadf4884cb8aec3
Tkg6161 <3425059946@qq.com>
2026-07-19 16:16:54 +0800

    scanf+

--- C_language_notes/scanf/scanf.c
+++ C_language_notes/scanf/scanf.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int num;
+    /*int num;
     printf("Enter an integer: ");
     scanf("%d", &num);
-    printf("You entered: %d\n", num);
-    return 0;
+    printf("You entered: %d\n", num);*/
+
+    /* int num1,num2;
+    printf("Enter two integers: ");
+    scanf("%d %d",&num1, &num2);
+    printf("%d +%d = %d",num1 ,num2 ,num1 + num2);
+*/
+    //scanf“”中的内容是要输入的内容，里面的空格是分隔，要是两个字符间的分隔才能满足；
+
+    int num1,num2;
+    printf("Enter two integers: ");
+    scanf("%d, %d",&num1, &num2);
+    printf("%d + %d = %d", num1, num2, num1 + num2);
+    return 0; 
 }
