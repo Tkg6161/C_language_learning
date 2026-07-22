261bf4e5f09af7a6cd3c1a67e15f09be8df66958
Tkg6161 <3425059946@qq.com>
2026-07-22 19:13:10 +0800

    exercise3

---
+++ C_language_exercises/3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    //没法处理0啊
+    int num =0;
+    scanf("%d",&num);
+    int key1= 10;
+    int key2= 1;
+    int key3= 1;
+    int dim =num%key1/key2;
+    while(dim!=0||num%key1==0)
+    {
+        key3*=10;
+        key1*=10;
+        key2*=10;
+        dim=num%key1/key2;
+
+    }
+    int sum=0;
+    key1= 10;
+    key2= 1;
+    key3/=10;
+    dim =num%key1/key2;
+    while(dim!=0||num%key1==0)
+    {
+        sum+=dim*key3;
+        key3/=10;
+        key1*=10;
+        key2*=10;
+        dim=num%key1/key2;
+    }
+    printf("%d",sum);
+    
+
+    
+    return 0;
+}
