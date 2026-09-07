e4e8e19133f1fd0280f6fe4256f6cdd9f39bc94c
Tkg6161 <3425059946@qq.com>
2026-09-07 17:18:00 +0800

    多种字符串输入方法的应用

--- C_language_exercises/统计输入字符串的单词数.c
+++ C_language_exercises/统计输入字符串的单词数.c
@@ -7,20 +7,27 @@ int main()
     //scanf("%s",string);
     //scanf 用%s 不能存中间带空格的字符串，会自动在第一个单词后加'\0'
 
-    for(int j=0;string[j]!='\0';j++)
+    //for(int j=0;string[j]!='\0';j++)
+    //{
+    //   scanf("%c",&string[j]);
+    //}
+    //不能用该循环终止逻辑，数组元素未初始化均为乱码，可能导致循环提前结束
+
+    char c;
+    int i;
+    for(i=0;(c=getchar())!='\n';i++)
     {
-        scanf("%c",&string[j]);
+        string[i]=c;
     }
-    printf("%s\n",string);
+    string[i]='\0';
+    //for循环输入字符串要手动在最后加'\0'
 
     for(int i=0;string[i]!='\0';i++)
     {
         if(string[i]>='a'&&string[i]<='z'||string[i]>='A'&&string[i]<='Z')
         {
-            printf("%c\n",string[i]);
             if(string[i+1]==' '||string[i+1]=='\0')
             {
-                printf("%c\n",string[i]);
                 count++;
             }
         }
